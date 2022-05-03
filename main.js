045f5925f938daeff42482225fa486403558b518
Jacob Whitehead <j.whitehead143@hotmail.co.uk>
2022-05-03 22:51:20 +0100

    Misc

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const path = require('path');
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600,
+    height: 500,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true
