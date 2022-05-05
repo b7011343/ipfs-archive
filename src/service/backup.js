bd2e49cec92de4e58b39c9335fc1e704d32964e0
Jacob Whitehead <j.whitehead143@hotmail.co.uk>
2022-05-06 00:05:04 +0100

    Implement upload logic

--- src/service/backup.js
+++ src/service/backup.js
@@ -1,21 +1,23 @@
 const CryptoJS = require('crypto-js');
 const fs = require('fs');
 const zip = require('bestzip');
+const { Web3Storage } = require('web3.storage');
 
-const encryptionPassword = 'Test123!';
+
+const apiKey = '3b4jk13l4j32412kj34nl1j4lk32j4ll4j23l4[f3094f0232ftq';
 
 const dirs = [
   "D://"
 ];
 
 const getDestinationFileName = () => {
-  return "";
+  return "test";
 };
 
-const compress = (srcDir, destinationName) => {
+const compress = (srcDir, destinationName) => { // TODO: May want to test zipping the file multiple times to decrease the storage space used
   zip({
     source: srcDir,
-    destination: './destination.zip'`./${destinationName}.zip`
+    destination: `./temp-compress/${destinationName}.zip`
   }).then(() => {
     console.log('Compression complete');
   }).catch((err) => {
@@ -23,15 +25,37 @@ const compress = (srcDir, destinationName) => {
   });
 };
 
+const uploadIPFS = async (file) => {
+  const storage = new Web3Storage({ token: apiKey });
+  const cid = await storage.put(file);
+  console.log('Content added with CID:', cid);
+  return cid;
+};
+
 const _backup = async (dir) => {
   const destinationName = getDestinationFileName();
+  const compressedDir = `./temp-compress/${destinationName}.zip`;
   await compress(dir, destinationName);
-  
+
+  let zipFile = '';
+  const compressedDirFileStream = fs.createReadStream(compressedDir, { encoding: 'base64' });
+
+  compressedDirFileStream.on('data', (chunk) => zipFile += chunk);
+
+  compressedDirFileStream.on('end', async () => {
+    const encryptedZipFile = CryptoJS.AES.encrypt(zipFile, apiKey).toString();
+    const cid = await uploadIPFS(encryptedZipFile);
+    
+    fs.unlink(compressedDir, (err) => {
+      if (err) throw err;
+    });
+  });
+
 };
 
 const backup = async () => {
   for (const dir of dirs) {
-
+    _backup(dir);
   }
 };
 
